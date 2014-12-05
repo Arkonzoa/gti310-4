581932d3cf572bce15f40f40670960cb72ea67fe
Davmira <davmurat@msn.com>
2014-12-05 10:20:34 -0500

    Float to int

--- SqueezeLightMediaCodec/gti310/tp4/RGBToYCbCr.java
+++ SqueezeLightMediaCodec/gti310/tp4/RGBToYCbCr.java
@@ -2,26 +2,33 @@ package gti310.tp4;
 
 /**
  * Convertie une image ppm RGB en YCbCr
+ * 
  * @author David
  *
  */
 public class RGBToYCbCr {
 
-	public static float[][][] convertir(int[][][] imageAConverir) {
-		
-		float[][][] newImage = new float[Main.COLOR_SPACE_SIZE][PPMReaderWriter.getM_height()][PPMReaderWriter.getM_width()];
+	public static int[][][] convertir(int[][][] imageAConverir) {
 
-		for(int i = 0; i < PPMReaderWriter.getM_height(); i++) {
-			for(int j = 0; j < PPMReaderWriter.getM_width() ; j++) {
-				newImage[Main.Y][i][j] = (float) (0.299*imageAConverir[Main.R][i][j])+(float) (0.587*imageAConverir[Main.R][i][j])+(float) (0.114*imageAConverir[Main.B][i][j])+0;
-				newImage[Main.Cb][i][j] = (float) ((float) (-0.168736*imageAConverir[Main.R][i][j])+(float) (-0.331264*imageAConverir[Main.R][i][j])+(float) (0.5*imageAConverir[Main.B][i][j])+0.5);
-				newImage[Main.Cr][i][j] = (float) ((float) (0.5*imageAConverir[Main.R][i][j])+(float) (-0.418688*imageAConverir[Main.R][i][j])+(float) (-0.081312*imageAConverir[Main.B][i][j])+0.5);
+		int[][][] newImage = new int[Main.COLOR_SPACE_SIZE][PPMReaderWriter
+				.getM_height()][PPMReaderWriter.getM_width()];
+
+		for (int i = 0; i < PPMReaderWriter.getM_height(); i++) {
+			for (int j = 0; j < PPMReaderWriter.getM_width(); j++) {
+				newImage[Main.Y][i][j] =  (int) ( (0.299 * imageAConverir[Main.R][i][j])
+						+  (0.587 * imageAConverir[Main.R][i][j])
+						+  (0.114 * imageAConverir[Main.B][i][j]) + 0);
+				newImage[Main.Cb][i][j] =   (int) ((-0.168736 * imageAConverir[Main.R][i][j])
+						+  (-0.331264 * imageAConverir[Main.R][i][j])
+						+ (0.5 * imageAConverir[Main.B][i][j]) + 0.5);
+				newImage[Main.Cr][i][j] =  (int) ((0.5 * imageAConverir[Main.R][i][j])
+						+  (-0.418688 * imageAConverir[Main.R][i][j])
+						+ (-0.081312 * imageAConverir[Main.B][i][j]) + 0.5);
 			}
 		}
-		
+
 		return newImage;
-		
-		
+
 	}
-	
+
 }
--- SqueezeLightMediaCodec/gti310/tp4/YCbCrToRGB.java
+++ SqueezeLightMediaCodec/gti310/tp4/YCbCrToRGB.java
@@ -1,22 +1,26 @@
 package gti310.tp4;
 
 public class YCbCrToRGB {
-	
-	public static float[][][] convertir(int[][][] imageAConverir) {
-		
-		float[][][] newImage = new float[Main.COLOR_SPACE_SIZE][PPMReaderWriter.getM_height()][PPMReaderWriter.getM_width()];
 
-		for(int i = 0; i < PPMReaderWriter.getM_height(); i++) {
-			for(int j = 0; j < PPMReaderWriter.getM_width() ; j++) {
-				newImage[Main.R][i][j] = (float) (1*imageAConverir[Main.Y][i][j])+(float) (0*imageAConverir[Main.R][i][j])+(float) (1.140*imageAConverir[Main.B][i][j]);
-				newImage[Main.G][i][j] = (float) ((float) (1*imageAConverir[Main.Cb][i][j])+(float) (-0.395*imageAConverir[Main.R][i][j])+(float) (-0.581*imageAConverir[Main.B][i][j]));
-				newImage[Main.B][i][j] = (float) ((float) (1*imageAConverir[Main.Cr][i][j])+(float) (2.032*imageAConverir[Main.R][i][j])+(float) (0*imageAConverir[Main.B][i][j]));
+	public static int[][][] convertir(int[][][] imageAConverir) {
+
+		int[][][] newImage = new int[Main.COLOR_SPACE_SIZE][PPMReaderWriter
+				.getM_height()][PPMReaderWriter.getM_width()];
+
+		for (int i = 0; i < PPMReaderWriter.getM_height(); i++) {
+			for (int j = 0; j < PPMReaderWriter.getM_width(); j++) {
+				newImage[Main.R][i][j] = (int) ((1 * imageAConverir[Main.Y][i][j])
+						+  (0 * imageAConverir[Main.R][i][j])
+						+  (1.140 * imageAConverir[Main.B][i][j]));
+				newImage[Main.G][i][j] =  (int) ((1 * imageAConverir[Main.Cb][i][j])
+						+  (-0.395 * imageAConverir[Main.R][i][j]) + (-0.581 * imageAConverir[Main.B][i][j]));
+				newImage[Main.B][i][j] =  (int) ((1 * imageAConverir[Main.Cr][i][j])
+						+  (2.032 * imageAConverir[Main.R][i][j]) +  (0 * imageAConverir[Main.B][i][j]));
 			}
 		}
-		
+
 		return newImage;
-		
-		
+
 	}
 
 }
